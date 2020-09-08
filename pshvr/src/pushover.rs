3ea49914ac8ee1b289f1c1538c780ccfd229add8
Hugo Lundin <hugo@lundin.dev>
2020-09-08 07:11:51 +0200

    Minor updates

--- pshvr/src/pushover.rs
+++ pshvr/src/pushover.rs
@@ -1,10 +1,10 @@
 use std::error::Error;
 
 extern crate dirs;
-use dirs::home_dir;
 
 extern crate envfile;
 use envfile::EnvFile;
+
 pub struct Pushover {
     user: String,
     token: String
@@ -19,25 +19,18 @@ impl Pushover {
     }
 
     pub fn from_config() -> Self {
-        let home = match home_dir() {
-            Some(d) => d,
-            _ => panic!("Unable to open home directory.")
-        };
-    
-        let config = match EnvFile::new(&home.join(".pshvrc")) {
-            Ok(c) => c, 
-            _ => panic!("~/.pshvrc does not exist.")
-        };
-    
-        let user = match config.get("user") {
-            Some(u) => u,
-            _ => panic!("'user' does not exist in ~/.pshvrc")
-        };
-    
-        let token = match config.get("token") {
-            Some(t) => t,
-            _ => panic!("'token' does not exist in ~/.pshvrc")
-        };
+        let path = dirs::home_dir()
+            .expect("Unable to open home directory.")
+            .join("pshvrc");
+
+        let config = EnvFile::new(&path)
+            .expect("~/.pshvrc does not exist.");
+
+        let user = config.get("user")
+            .expect("'user' does not exist in ~/.pshvrc");
+
+        let token = config.get("token")
+            .expect("'token' does not exist in ~/.pshvrc");
     
         Pushover::new(user.to_string(), token.to_string())
     }
